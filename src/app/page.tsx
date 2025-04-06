7294116402febbb03dfbdccb8355e56c8e668e5a
dvankevich <dvankevich@gmail.com>
2025-04-06 19:07:20 +0300

    Client Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // src/page.tsx
-import StatusLabel, { Status } from '@/app/components/status-label';
+import AddCompanyButton from '@/app/components/add-company-button';
 import { headers } from 'next/headers';
 
 export default function Home() {
@@ -9,11 +9,8 @@ export default function Home() {
   });
   return (
     <main>
-      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <h1 className="text-xl">Home page</h1>
+      <AddCompanyButton />
     </main>
   );
 }
