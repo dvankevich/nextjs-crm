31114a4320abff750426a08bac6700dd5f2a264b
dvankevich <dvankevich@gmail.com>
2025-04-06 13:35:40 +0300

    Static and Dynamic Rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+// src/page.tsx
 import StatusLabel, { Status } from '@/app/components/status-label';
+import { headers } from 'next/headers';
 
 export default function Home() {
+  const headerList = headers();
+  headerList.forEach((value, key) => {
+    console.log(`${key} ==> ${value}`);
+  });
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
